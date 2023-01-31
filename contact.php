90cf7b40239f007001295900fbf68be10a11ece9
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-01-31 18:23:21 -0500

    Created Contact Page/Edited Header&footer

--- contact.php
+++ contact.php
@@ -3,6 +3,59 @@
 ?>
 
 
+<div class="container"> 
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-sm-10 col-md-3 mt-4">
+            <h1 class="">
+
+                <span>Contact Us</span>
+
+            </h1>
+        </div>
+        <div class="col-md-3"></div>
+        <div class="col-sm-10 col-md-4  mt-5">
+            <nav class="" aria-label="You are here:"><span class="" itemscope="" itemtype="http://schema.org/BreadcrumbList">
+                <span class="" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                    <a itemprop="item" href="home.php" rel="home">
+                        <span itemprop="name">Home
+
+                        </span>
+                    </a>
+                    <meta itemprop="position" content="1">
+                </span>
+                <span class="sep sep-1"> » </span>
+                <span class="trail-end">Contact Us</span></span>
+            </nav>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 mt-5">
+            <h5 class="text-dark mx-3">Got Any Questions?</h5>
+        </div>
+        <div class="col-md-2"></div>
+    </div>   
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 mt-5">
+            <p class="text-dark mx-3">Any questions, feedback, or looking to contribute? We would love to hear from you! </p>
+        </div>
+        <div class="col-md-2"></div>
+    </div>     
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 mt-5">
+            <p class="text-dark mx-3">To get in touch, please fill out the form below. </p>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+</div>
+         
+
+    
+
+
 <?php
       require_once("components/form.html");
 ?>
--- readme.txt
+++ readme.txt
@@ -5,4 +5,4 @@ Time: 10:00am-2:00pm
 -   using bootstrap 5 I created the header and footer 
     components for the project -> both are mobile friendly
 -   recreatd the newsletter form
-- 
+-   created mini directory navigation for users to get back to their last screen
