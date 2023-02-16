346c6d1a44c26df47db41d5660cddda338ec07ba
smaugggg <roborant@gmail.com>
2023-02-16 00:19:45 -0500

    pulled main to mess with, commit to my branch

---
+++ 000/about.php
@@ -0,0 +1,131 @@
+<?php
+      require_once("app/Layouts/header.php");
+?>
+
+
+    <div class="container-lg tests">
+        <div class="row mt-4">
+            <div class="col-sm-12 col-md-12 col-lg-4 left-nav">
+                <h4 class="content-nav-header">On This Page</h4>
+                <ul style="padding:0;">
+                    <li class="content-nav-item"><a href="#">Who We are</a></li>
+                    <li class="content-nav-item"><a href="#">What We Do</a></li>
+                    <li class="content-nav-item"><a href="#">Supporters</a></li>
+                    <li class="content-nav-item"><a href="#">Network FAQs</a></li>
+                    <li class="content-nav-item"><a href="#">Why It Matters</a></li>
+                    <li class="content-nav-item"><a href="#">Collective Impact</a></li>
+                    <li class="content-nav-item"><a href="#">Strategy Roadmap</a></li>
+                    <li class="content-nav-item"><a href="#">Outcomes & Capabilities</a></li>
+                    <li class="content-nav-item"><a href="#">Action</a></li>
+                    <li class="content-nav-item"><a href="#">Impact</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-1"></div>
+            <div class="col-sm-12 col-md-12 col-lg-7">
+                <section id="News">
+                    <h3 class="my-2">News</h3><br>
+                    <p class="mb-4">The Calgary Parks Foundation is exploring the concept of a
+                        Dementia-Inclusive Park in Calgary.  This would be the
+                        first public dementia park in Canada!  Click <a href="https://www.albertaprimetimes.com/beyond-local/canadas-first-dementia-inclusive-park-coming-to-calgary-6318318">HERE</a> to
+                        read Alberta Prime Time's article.
+                    </p>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <a href="https://www.cbc.ca/news/canada/calgary/dementia-small-scale-family-caregivers-1.6348524" target="_blank">
+                                <img src="img/CBC.png" alt="Dementia Network CBC Article" >
+                            </a>
+                        </div>
+                        <div class="col-md-7">
+                            <a href="https://dementianetworkcalgary.ca/wp-content/uploads/2022/03/A-summary-aducanumab-statement.pdf" target="_blank">
+                                <img src="img/statementLogo.png" width="75%" alt="Dementia Network Calgary Statement" >
+                            </a>
+                        </div>
+                        <div class="col-md-2"></div>
+                    </div>
+                </section>
+                <section id="WhatIsDementia">
+                    <h3 class="mt-5 mb-3">What Is Dementia?</h3><br>
+                    <h6>Looking For Further Information?</h6><br>
+                    <p>Check out the links below. If you can't find the information you're looking for, send us a message <a href="contact.php">here</a>.</p>
+                    <div class="container-lg">
+                        <div class="row">
+                            <div class="col-md-6 my-3">
+                                <h5>Tools Via Action Awareness Team</h5><br>
+                                <a class="mt-2" href="https://dementianetworkcalgary.ca/wp-content/uploads/2022/06/CommunicatingandEngaginghandout.pdf" target="_blank" >Communicating and Engaging With Someone Who Has Dementia</a><br>
+                                <a class="mt-2" href="https://dementianetworkcalgary.ca/wp-content/uploads/2022/06/Letmereintroducemyselftoolkit.pdf" target="_blank">Let Me Re-Introduce Myself</a><br>
+                            </div>
+                            <div class="col-md-6 my-3">
+                                <h5>Facts About Dementia</h5><br>
+                                <a href="https://www.alzheimercalgary.ca/learn/what-you-need-to-know" class="mt-2"  target="_blank">What You Need To Know</a><br>
+                                <a href="https://www.alzheimercalgary.ca/learn/what-you-need-to-know" class="mt-2"  target="_blank">Alzheimers Disease vs Dementia</a><br>
+                                <a href="https://alzheimer.ca/en/About-dementia/What-is-dementia" class="mt-2"  target="_blank">Facts About Dementia</a><br>
+                            </div>
+                            <div class="col-md-6 my-3">
+                                <h5>Common Myths</h5><br>
+                                <a href="https://alzheimer.ca/en/About-dementia/Alzheimer-s-disease/Myth-and-reality-about-Alzheimer-s-disease" class="mt-2"  target="_blank">Broken Link 1</a><br>
+                                <a href="https://alzheimer.ca/~/media/Files/national/Core-lit-brochures/Dispelling_Myths_e.pdf" class="mt-2"  target="_blank">Broken Link 2</a><br>
+                            </div>
+                            <div class="col-md-6 my-3">
+                                <h5>FAQS About Dementia</h5><br>
+                                <a href="https://www.alzheimercalgary.ca/learn/common-questions" class="mt-2"  target="_blank">Common Questions</a><br>
+                                <a href="http://www.alzheimer.ca/en/About-dementia/Alzheimer-s-disease/Common-Questions" class="mt-2"  target="_blank">Broken Link 3</a><br>
+                            </div>
+                            <div class="col-md-6 my-3">
+                                <h5>General Dementia Resources</h5><br>
+                                <a href="https://www.alzheimercalgary.ca/" class="mt-2"  target="_blank">Alzheimer Calgary</a><br>
+                                <a href="https://www.alzint.org/" class="mt-2"  target="_blank">Alzheimer's Disease International</a><br>
+                            </div>
+                            <div class="col-md-6 my-3">
+                                <h5>Living With Dementia</h5><br>
+                                <a href="https://www.alzheimercalgary.ca/find-support/just-diagnosed" class="mt-2"  target="_blank">Recently Diagnosed?</a><br>
+                                <a href="https://www.alzheimercalgary.ca/find-support/things-to-consider-if-you-have-dementia" class="mt-2"  target="_blank">Things to Consider</a><br>
+                                <a href="https://www.alzheimercalgary.ca/find-support/caring-for-someone-with-dementia" class="mt-2"  target="_blank">Caring for Someone With Dementia</a><br>
+                                <a href="https://alzheimer.ca/en/Living-with-dementia" class="mt-2"  target="_blank">Broken Link</a><br>
+                                <a href="https://www.cmhc-schl.gc.ca/en" class="mt-2"  target="_blank">Potentially Unrelated?</a><br>
+                                <a href="https://dementianetworkcalgary.ca/s/Human-Rights-for-People-Living-with-Dementia-Rhetoric-to-Reality.pdf" class="mt-2"  target="_blank">Broken Link</a><br>
+                            </div>
+                            <div class="col-md-6 my-3">
+                                <h5>Tips and Toolkits</h5><br>
+                                <a href="https://alzheimer.ca/en/Living-with-dementia/Ways-to-communicate/Tips-for-communicating-person-with-dementia" class="mt-2"  target="_blank">Broken Link</a><br>
+                                <a href="https://alzheimer.ca/en/Living-with-dementia/Staying-connected/Visiting-someone-with-dementia" class="mt-2"  target="_blank">Broken Link</a><br>
+                                <a href="https://alzheimer.ca/en/Living-with-dementia/Staying-connected/Holidays-and-special-occasions" class="mt-2"  target="_blank">Broken Link</a><br>
+                                <a href="https://alzheimer.ca/en/Living-with-dementia/Staying-connected/Helping-children" class="mt-2"  target="_blank">Broken Link</a><br>
+                                <a href="https://alzheimer.ca/en/Living-with-dementia/Staying-connected/Helping-teens" class="mt-2"  target="_blank">Broken Link</a><br>
+                            </div>
+                            <div class="col-md-6 my-3">
+                                <h5>General Dementia Resources</h5><br>
+                                <a href="https://www.alzheimercalgary.ca/" class="mt-2"  target="_blank">Alzheimer Calgary</a><br>
+                                <a href="https://www.alzint.org/" class="mt-2"  target="_blank">Alzheimer's Disease International</a><br>
+                            </div>
+                            <div class="col-md-6 my-3">
+                                <h5>Recognizing Dementia</h5><br>
+                                <a href="https://www.alzheimercalgary.ca/learn/warning-signs" class="mt-2"  target="_blank">Warning Signs</a><br>
+                            </div>
+                            <div class="col-md-6 my-3">
+                                <h5>Latest Research</h5><br>
+                                <a href="https://www.alzheimercalgary.ca/learn/cause-and-cure" class="mt-2"  target="_blank">Alzheimer Calgary</a><br>
+                                <a href="https://www.alzint.org/what-we-do/research/" class="mt-2"  target="_blank">Alzheimer's Disease International</a><br>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-2"></div>
+                                <div class="col-md-8 my-3">
+                                    <h5>Dementia Friends Canada</h5><br>
+                                    <p class="text-dark">A national awareness and public engagement initiative with an aim is to create a more aware and informed Canadian population to dispel myths and reduce stigma about dementia. Find more information <a href="https://www.dementiafriends.ca/">here</a>.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!--  -->
+                </section>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+<?php
+      require_once("app/Layouts/footer.html");
+?>
---
+++ 000/contact.php
@@ -0,0 +1,37 @@
+<?php
+      require_once("app/Layouts/header.php");
+?>
+
+
+<div class="container"> 
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 mt-5">
+            <h5 class="text-dark my-4 mx-3">Got Any Questions?</h5>
+            <p class="text-dark my-4 mx-3">
+                Any questions, feedback, or looking to contribute?
+                We would love to hear from you!
+            </p>
+            <p class="text-dark my-4 mx-3">To get in touch, please fill out the form below. </p>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+</div>
+         
+
+    
+
+
+<?php
+      require_once("app/Layouts/form.html");
+?>
+
+
+
+
+
+
+
+<?php
+      require_once("components/footer.html");
+?>
