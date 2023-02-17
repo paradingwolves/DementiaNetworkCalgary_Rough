2544b1f5cb4f59d54cf9e3b8f5e0c2c26843bf3a
smaugggg <roborant@gmail.com>
2023-02-16 23:54:16 -0500

    highlight banner

---
+++ 000/app/Layouts/banner.php
@@ -0,0 +1,17 @@
+<!-- This is the highlight banner for the top of the site -->
+<div class="highlight-frame">
+    <div class="row gx-5">
+        <div class="col-sm-12 col-md-1 col-lg-1 col-xl-1"></div>
+        <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3 highlight-info">
+            <h4>EVENT TITLE</h4>
+            <img src="app/img/highlightbanner/20230216-2234-001.png" alt="">
+        </div>
+        <div class="col-sm-12 col-md-7 col-lg-7 col-xl-7 highlight-desc">
+            <p>Dementia Network Calgary and the Alzheimers Society of Calgary want to know what you really think.
+                We are asking important questions to understand what you really think and feel about Alzheimer’s
+                disease and related dementias. There’s zero judgement and it’s completely anonymous. Your voice will
+                help us develop the right plans to build greater awareness and understanding in our community. Thank
+                you so much for taking a few brief moments to share your thoughts.</p>
+        </div>
+    </div>
+</div>
